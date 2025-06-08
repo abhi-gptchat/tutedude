3cf9fca9d0b30b966ecc45b3e109e69413239978
abhi-gptchat <abhi.gptchat@gmail.com>
2025-06-08 13:45:40 +0100

    Create task1.py

---
+++ Assignment1/task1.py
@@ -0,0 +1,7 @@
+n1 = int(input('Enter the first number: '))
+n2 = int(input('Enter the second number: '))
+
+print(f'Addition: ', n1 + n2)
+print(f'Subtraction: ', n1 - n2)
+print(f'Multiplication: ', n1 * n2)
+print(f'Division: ', n1/n2)
