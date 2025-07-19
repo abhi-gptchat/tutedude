be49a2ad401320b2efac1da48775ada26fb72dc9
abhi-gptchat <abhi.gptchat@gmail.com>
2025-07-19 08:54:20 +0100

    Create task2.py

---
+++ Assignment5/task2.py
@@ -0,0 +1,10 @@
+numbers = list(range(1,11))
+
+extracted = numbers[0:5]
+
+print('Original list: ', numbers)
+print("Extracted first five elements: ", extracted)
+
+extracted.reverse()
+
+print('Reverse extracted elements: ', extracted)
